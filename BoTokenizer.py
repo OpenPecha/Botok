f97c12dbc7b32dc5813c0c84eea21133ceea52f4
drupchen <hhdrupchen@gmail.com>
2018-02-24 00:09:58 +0100

    comments added, non-max-match correctly supported

--- BoTokenizer.py
+++ BoTokenizer.py
@@ -67,95 +67,138 @@ class Tokenizer:
         self.WORD = 1000
         self.NON_WORD = 1001
 
-    def tokenize(self):
+    def tokenize(self, debug=False):
         tokens = []
-        word = []
+        syls = []
 
         current_node = None
         went_to_max = False
+        match_idx = -1
 
         c_idx = 0
         while c_idx < len(self.pre_processed.chunks):
             has_decremented = False
             is_non_word = False
             chunk = self.pre_processed.chunks[c_idx]
-            if chunk[0]:  # chunk is a syllable
 
+            # 1. CHUNK IS SYLLABLE
+            if chunk[0]:
+                # syl is extracted from input string, tsek added for the trie
                 syl = [self.pre_processed.string[idx] for idx in chunk[0]] + ['་']
+                self.debug(debug, syl)
 
+                # >>> WALKING THE TRIE >>>
                 s_idx = 0
                 while s_idx <= len(syl)-1:
+                    # beginning of current syllable
                     if s_idx == 0:
-                        char = syl[s_idx]
+                        self.debug(debug, syl[s_idx])
+
+                        # begining of current word
                         if not current_node:
                             current_node = self.trie.walk(syl[s_idx], self.trie.head)
+                            if current_node and current_node.is_match:
+                                match_idx = c_idx
+
+                        # walking resumed after previous syllable
                         else:
                             current_node = self.trie.walk(syl[s_idx], current_node)
+                            if current_node and current_node.is_match:
+                                match_idx = c_idx
                         s_idx += 1
 
+                    # continuing to walk
                     elif current_node and current_node.can_walk:
-                        char = syl[s_idx]
+                        self.debug(debug, syl[s_idx])
                         current_node = self.trie.walk(syl[s_idx], current_node)
+                        if current_node and current_node.is_match:
+                            match_idx = c_idx
+                # <<<<<<<<<<<<<<<<<<<<<<<<
 
-                        if not current_node and word:
+                        if not current_node and syls:
                             if not has_decremented:
                                 c_idx -= 1
                                 has_decremented = True
                             went_to_max = True
                         s_idx += 1
 
+                    # CAN'T CONTINUE WALKING
                     else:
-                        # we couldn't go until the end of the syl
-                        if word:
+
+                        # a. potential word(syls) is not empty
+                        if syls:
+                            # need to finish looping over current syl
                             if went_to_max:
                                 s_idx += 1
                                 continue
 
+                            # couldn't walk this syl until the end.
+                            # decrementing chunk-idx for a new attempt to find a match
                             if not has_decremented:
                                 c_idx -= 1
-                                has_decremented = True
+                                has_decremented = True  # ensures we only decrement once per syl
                             went_to_max = True
+
+                        # b. current word is empty
                         else:
                             # there is only a non-word
                             is_non_word = True
                         s_idx += 1
 
+                # Finished looping over current syl.
                 if is_non_word:
-                    # non-words are left as syllables
+                    # non-word syls are turned into independant tokens
                     non_word = syl
                     tokens.append(non_word)
 
                 else:
+
                     if went_to_max:
-                        if word and not has_decremented:
+                        if not has_decremented:
                             c_idx -= 1
 
                         else:
-                            tokens.append(word)
-                            word = []
+                            c_idx = self.there_are_syls_process(c_idx, match_idx, syls, tokens)
+                            syls = []
                         went_to_max = False
 
                     else:
-                        word.append(syl)
+                        syls.append(syl)
 
+            # 2. CHUNK IS NON-SYLLABLE
             else:
                 # if there is a word that was not added
-                if word:
-                    tokens.append(word)
-                    word = []
+                if syls:
+                    c_idx = self.there_are_syls_process(c_idx, match_idx, syls, tokens)
+                    syls = []
                     current_node = None
 
-                # is non-bo, add it to tokens as a Word
                 token = [[self.pre_processed.string[idx] for idx in range(chunk[1][1], chunk[1][1]+chunk[1][2])]]
                 tokens.append(token)
 
             c_idx += 1
 
-        if word:
-            tokens.append(self.word)
+        # a potential token was left
+        if syls:
+            self.there_are_syls_process(c_idx, match_idx, syls, tokens)
 
-        self.reinitialize_vars()
         return tokens
+
+    @staticmethod
+    def there_are_syls_process(c_idx, match_idx, syls, tokens):
+        # there is a match
+        if match_idx == c_idx:
+            word = syls
+            tokens.append(word)
+        else:
+            # add first syl in syls as non-word
+            tokens.append(syls[0])
+            del syls[0]
+
+            # decrement chunk-idx for a new attempt to find a match
+            if syls:
+                c_idx -= len(syls) - 1
+        return c_idx
 
     def create_token(self, ttype, start, length, syls, pos=None):
         token = Word()
@@ -174,23 +217,23 @@ class Tokenizer:
         token.char_groups = self.pre_processed.export_groups(start, length, for_substring=True)
         return token
 
-    def reinitialize_vars(self):
-        self.tokens = []
-        self.word = []
-        # self.word_start = -1
-        # self.word_len = -1
-        self.non_word = []
-        # self.non_word_start = -1
-        # self.non_word_len = -1
+    @staticmethod
+    def debug(debug, to_print):
+        if debug:
+            print(to_print)
 
 
 if __name__ == '__main__':
     """example use"""
     input_string = ' ཤི་བཀྲ་ཤིས་  tr བདེ་་ལེ གས། བཀྲ་ཤིས་བདེ་ལེགས་ཀཀ'
-    test = 'ཀཀ་ཀཀ་ཞེས་བྱ་བ། ངེད་རྣམས་ནི་མཐོ་གོ་གནས་དང་། འབྱོར་ལྡན། མིང་གྲགས་ཡོད་མཁན་དེ་འདྲ་ནམ་ཡང་མིན་ལ། ཅི་ཞིག་ཤེས་ཁུལ་གྱིས་ཚོགས་པ་འདི་གཉེར་བ་ཞིག་ཀྱང་གཏན་ནས་མིན། ཚན་རྩལ་གྱི་རྦ་རླབས་དྲག་ཏུ་འཕྱོ་ལྡིང་བྱེད་པའི་དུས་སྐབས་འདིར་ང་ཚོས་སྔ་ས་ནས་རང་གི་སྐད་ཡིག་དང་རིག་གཞུང་ལ་དུང་བ་ཞིག་ན་གཞོན་ཚོར་བསྐྲུན་མ་ཐུབ་པ་དང་། བདག་གཅེས་ལ་འབད་མ་ནུས་ཚེ་ཡོད་ཚད་མིང་ཙམ་ཞིག་ཏུ་གྱུར་ཚར་རྗེས་ངལ་བ་ཇི་ཙམ་བརྟེན་ཡང་སྙིང་པོ་ལོན་རྒྱུ་དཀའ་བས་ད་ལྟ་མ་སྔ་མ་ཕྱིས་བའི་དུས་ཚིགས་གལ་ཆེན་ཞིག་ཏུ་བརྩིས་ནས་ང་ཚོས་འདི་ལྟར་རང་ནུས་ལ་དཔགས་པའི་སྐད་ཡིག་རིག་གཞུང་དང་འབྲེལ་བའི་བྱེད་སྒོ་སྤེལ་མུས་ཡིན།'
+    test = 'ཀཀ་ཀཀ་ཞེས་བྱ་བ། ངེད་རྣམས་ནི་མཐོ་གོ་གནས་དང་། འབྱོར་ལྡན། མིང་གྲགས་ཡོད་མཁན་དེ་འདྲ་ནམ་ཡང་མིན་ལ། ' \
+           'ཅི་ཞིག་ཤེས་ཁུལ་གྱིས་ཚོགས་པ་འདི་གཉེར་བ་ཞིག་ཀྱང་གཏན་ནས་མིན། ཚན་རྩལ་གྱི་རྦ་རླབས་དྲག་ཏུ་འཕྱོ་ལྡིང་བྱེད་' \
+           'པའི་དུས་སྐབས་འདིར་ང་ཚོས་སྔ་ས་ནས་རང་གི་སྐད་ཡིག་དང་རིག་གཞུང་ལ་དུང་བ་ཞིག་ན་གཞོན་ཚོར་བསྐྲུན་མ་ཐུབ་པ་དང་། ' \
+           'བདག་གཅེས་ལ་འབད་མ་ནུས་ཚེ་ཡོད་ཚད་མིང་ཙམ་ཞིག་ཏུ་གྱུར་ཚར་རྗེས་ངལ་བ་ཇི་ཙམ་བརྟེན་ཡང་སྙིང་པོ་ལོན་རྒྱུ་དཀའ་བས་ད་' \
+           'ལྟ་མ་སྔ་མ་ཕྱིས་བའི་དུས་ཚིགས་གལ་ཆེན་ཞིག་ཏུ་བརྩིས་ནས་ང་ཚོས་འདི་ལྟར་རང་ནུས་ལ་དཔགས་པའི་སྐད་ཡིག་རིག་གཞུང་དང་' \
+           'འབྲེལ་བའི་བྱེད་སྒོ་སྤེལ་མུས་ཡིན།'
 
     tok = Tokenizer(test)
     words = tok.tokenize()
     for w in words:
         print(''.join([''.join(a) for a in w]), end=' ')
-
