59cc50ae5e957493e6e71e83cf40373bbbbfeb09
drupchen <hhdrupchen@gmail.com>
2018-03-26 23:08:56 +0200

    update

--- pybo/__init__.py
+++ pybo/__init__.py
@@ -1,7 +1,6 @@
 from .BoTrie import PyBoTrie
 from .BoStringUtils import PyBoTextChunks
 from .BoTokenizer import Tokenizer
-from .BoTokenUtils import TokenSplit
 from .BoSylUtils import BoSyl
 
-__all__ = ['PyBoTextChunks', 'PyBoTrie', 'Tokenizer', 'TokenSplit', 'BoSyl']
+__all__ = ['PyBoTextChunks', 'PyBoTrie', 'Tokenizer', 'BoSyl']
