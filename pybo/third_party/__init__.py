b8e40df85186933a40cfb60c7e3fc99386f6415e
drupchen <hhdrupchen@gmail.com>
2018-04-26 11:24:04 +0200

    implement ability to replace bool and int values

--- pybo/third_party/__init__.py
+++ pybo/third_party/__init__.py
@@ -5,18 +5,33 @@ from .cql import Query
 __all__ = ['Query', 'parse_cql_query']
 
 
-def parse_cql_query(query):
+def parse_cql_query(query, numerals=True, booleans=True):
     """
     CQL parser for replacing the content of Token.attributes.
     From the CQL syntax, only the attribute names and the values
     are taken into account.
 
     :param query: CQL query string
+    :param numerals: if True, gives the Python int instead of the string
+    :param booleans: if True, gives the Python bool instead of the string
     :return: a list of dicts, one per token slot, where
                 keys == Token.attributes and
                 values == content of the expected Token.attributes
     """
-    def cql2dict(tokenexpr):
+    def str2int(string):
+        try:
+            return int(string)
+        except ValueError:
+            return string
+
+    def str2bool(string):
+        b = {'True': True, 'False': False, 'None': None}
+        if string in b:
+            return b[string]
+        else:
+            return string
+
+    def cql2pattern(tokenexpr, numerals, booleans):
         """
         Expects the following syntax:
             '[attribute1="value1" & attribute2="value2" (& ...)]'
@@ -25,14 +40,18 @@ def parse_cql_query(query):
         for attrexprs in tokenexpr:
             key = attrexprs.attribute
             value = attrexprs.valueexpr[0]
+            if numerals:
+                value = str2int(value)
+            if booleans:
+                value = str2bool(value)
             changes[key] = value
         return changes
 
     if query:
         parsed = Query(query)
-        cql_dict = []
+        pattern = []
         for tokenexpr in parsed.tokenexprs:
-            cql_dict.append(cql2dict(tokenexpr))
-        return cql_dict
+            pattern.append(cql2pattern(tokenexpr, numerals, booleans))
+        return pattern
     else:
         return None
--- tests/test_matchers.py
+++ tests/test_matchers.py
@@ -1,7 +1,7 @@
 from pybo import *
 
 
-input_str = ' ཤི་བཀྲ་ཤིས་  tr བདེ་་ལེ གས། བཀྲ་ཤིས་བདེ་ལེགས་ཀཀ'
+input_str = ' མཐའི་རྒྱ་མཚོའི་གླིང་། ཤི་བཀྲ་ཤིས་  tr བདེ་་ལེ གས། བཀྲ་ཤིས་བདེ་ལེགས་ཀཀ'
 tok = BoTokenizer('POS')
 tokens = tok.tokenize(input_str)
 
@@ -11,15 +11,16 @@ def test_cql():
     matcher = CQLMatcher(query)
     slices = matcher.match(tokens)
     slice_strings = [tuple([tokens[i].content for i in range(start, end + 1)]) for start, end in slices]
-    assert slices == [(1, 2), (3, 4), (5, 6), (6, 7)]
-    assert slice_strings == [('བཀྲ་ཤིས་  ', 'tr'), (' བདེ་་ལེ གས', '།'), (' བཀྲ་ཤིས་', 'བདེ་ལེགས་'), ('བདེ་ལེགས་', 'ཀཀ')]
+    assert slices == [(0, 1), (2, 3), (4, 5), (7, 8), (9, 10), (11, 12), (12, 13)]
+    assert slice_strings == [('མཐ', 'འི་'), ('རྒྱ་མཚོ', 'འི་'), ('གླིང་', '།'), ('བཀྲ་ཤིས་  ', 'tr'),
+                             (' བདེ་་ལེ གས', '།'), (' བཀྲ་ཤིས་', 'བདེ་ལེགས་'), ('བདེ་ལེགས་', 'ཀཀ')]
 
 
 def test_token_split():
-    ts = TokenSplit(tokens[0], 1, token_changes='[tag="XXX" & pos="xxx"] []')
+    ts = TokenSplit(tokens[0], 1, token_changes='[tag="SPACE" & pos="PUNCT" & aa_word="False" & affixed="False"] []')
     first, second = ts.split()
-    assert first
-    assert second
+    assert first.tag == 'SPACE'
+    assert first.pos == 'PUNCT'
 
 
 def test_token_merge():
