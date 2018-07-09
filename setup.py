a44e4762b55286c5b9f35d3cf235db077738c297
Elie Roux <elie.roux@telecom-bretagne.eu>
2018-07-09 09:26:11 +0200

    fix pandoc requirement

--- setup.py
+++ setup.py
@@ -6,26 +6,16 @@ from __future__ import print_function
 import os
 import sys
 from setuptools import setup, find_packages
-
+import pypandoc
 
 def read(fname):
-    fname_rst = fname.replace('.md', '.rst')
-    if os.path.exists(fname_rst):
-        return open(os.path.join(os.path.dirname(__file__), fname_rst), encoding='utf-8').read()
-    else:
-        try:
-            import pypandoc
-            rst = pypandoc.convert(os.path.join(os.path.dirname(__file__), fname), 'rst')
-            with open(fname_rst, 'w') as f:
-                f.write(rst)
-            return rst
-        except (IOError, ImportError):
-            return open(os.path.join(os.path.dirname(__file__), fname), encoding='utf-8').read()
+    rst = pypandoc.convert(os.path.join(os.path.dirname(__file__), fname), 'rst')
+    return rst
 
 
 setup(
     name="pybo",
-    version="0.2.0",  #edit version in __init__.py
+    version="0.2.0",  # also edit version in pybo/__init__.py
     author="Esukhia development team",
     author_email="esukhiadev@gmail.com",
     description="Python utils for processing Tibetan",
@@ -34,7 +24,6 @@ setup(
     url="https://github.com/Esukhia/pybo",
     packages=find_packages(),
     long_description=read('README.md'),
-    long_description_content_type="text/markdown",
     project_urls={
         'Source': 'https://github.com/Esukhia/pybo',
         'Tracker': 'https://github.com/Esukhia/pybo/issues',
