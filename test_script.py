644a91aaae3ee8f8cd406d389a0e65b7d56f87cb
drupchen <hhdrupchen@gmail.com>
2019-06-27 23:12:10 +0200

    update coveralls.io

--- test_script.py
+++ test_script.py
@@ -1,70 +1,37 @@
-class ValidityError(Exception):
-    '''Raised when a validity check is not passed'''
+from .tests import *
 
-from collections import Counter
-import pybo as bo
-
-# 1. PREPARATION
-
-# 1.1. Initializing the tokenizers
-tok = bo.WordTokenizer('POS')
-
-# 1.2. Loading in text
-input_str = '༄༅། །རྒྱ་གར་སྐད་དུ། བོ་དྷི་སཏྭ་ཙརྻ་ཨ་བ་ཏ་ར། བོད་སྐད་དུ། བྱང་ཆུབ་སེམས་དཔའི་སྤྱོད་པ་ལ་འཇུག་པ། །སངས་རྒྱས་དང་བྱང་ཆུབ་སེམས་དཔའ་ཐམས་ཅད་ལ་ཕྱག་འཚལ་ལོ། །བདེ་གཤེགས་ཆོས་ཀྱི་སྐུ་མངའ་སྲས་བཅས་དང༌། །ཕྱག་འོས་ཀུན་ལའང་གུས་པར་ཕྱག་འཚལ་ཏེ། །བདེ་གཤེགས་སྲས་ཀྱི་སྡོམ་ལ་འཇུག་པ་ནི། །ལུང་བཞིན་མདོར་བསྡུས་ནས་ནི་བརྗོད་པར་བྱ། །'
-
-# -------------------------
-
-# 2. CREATING THE OBJECTS
-
-# 2.1. creating pre_processed object
-pre_processed = bo.TokChunks(input_str)
-
-# 2.2. creating tokens object
-tokens = tok.tokenize(input_str)
-
-# -------------------------
-
-# 3. TESTING ALL CLASS OBJECT ATTRIBUTES
-# (this needs to be checking accuracy and not just function)
-
-# 3.1. testing pre processed attributes
-for a in dir(pre_processed):
-    if not a.startswith('__'):
-        print(getattr(pre_processed, a))
-
-# 3.2. testing tokens attributes
-for a in dir(tokens):
-    if not a.startswith('__'):
-        getattr(tokens, a)
-
-# -------------------------
-
-# 4. FUNCTIONALITY TESTING
-
-# 4.1. accessing single token
-tokens[0]
-
-# 4.2. accessing content in all tokens
-[t.text for t in tokens]
-
-# 4.3. accessing pos in all tokens
-[t.pos for t in tokens]
-
-# 4.4. accessing tags that are nouns in all tokens
-[t.text for t in tokens if t.pos == 'NOUN']
-
-# 4.5. separating content based on tags and compiling into one list
-
-pos = set([t.pos for t in tokens])
-l = []
-for p in pos:
-    l.append([t.text for t in tokens if t.pos])
-
-# -------------------------
-
-# 5. VALIDITY TESTING
-
-# 5.1. length of output
-expected = 9
-if len(l) != expected:
-    raise ValidityError('Test 5.1. failed with list length ' + str(len(l)) + ' instead of the expected ' + str(expected))
+test_trie()
+test_tokenize()
+test_token()
+test_config()
+test_chunktokenizer()
+test_chunks()
+test_bosyl()
+test_papomerge()
+test_non_max2()
+test_adjust_tokens()
+test_bo_nonbo()
+test_cjk()
+test_components()
+test_cql()
+test_cql_query()
+test_createtrie()
+test_full_example()
+test_get_default_lemma()
+test_last_token()
+test_latin()
+test_match_merge()
+test_match_replace()
+test_match_split()
+test_non_max_end_of_string()
+test_num_nonnum()
+test_other()
+test_punct_nonpunct()
+test_space_nonspace()
+test_split_token()
+test_string()
+test_sym_nonsym()
+test_text()
+test_tokchunks()
+test_token_merge()
+test_token_split()
---
+++ tests/__init__.py
@@ -0,0 +1,17 @@
+# coding: utf8
+
+# this file is for using tests showcasing functionality as coverage tests
+from .test_basictrie import *
+from .test_bostring import *
+from .test_bosyl import *
+from .test_chunkframework import *
+from .test_chunks import *
+from .test_chunktokenizer import *
+from .test_config import *
+from .test_matchers import *
+from .test_splitaffixed import *
+from .test_sylcomponents import *
+from .test_token import *
+from .test_tokenize import *
+from .test_trie import *
+from .test_wordtokenizer import *
