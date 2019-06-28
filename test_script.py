cdf5b7e540a0415409c7cb975748c773afb90a5f
drupchen <hhdrupchen@gmail.com>
2019-06-28 09:50:43 +0200

    removing test for config.py

--- test_script.py
+++ test_script.py
@@ -421,31 +421,31 @@ assert tokens == [('TEXT', ' ཤི་'),
                   ('CJK', ' 这是  什么 '),
                   ('OTHER', 'กขฃคฅฆงจฉชซฌญฎฏฐฑฒณดตถทธ')]
 
-##########################################################################################################
-# test_config
-##########################################################################################################
-
-# def test_config():
-config = Config()
-
-# default config filename
-assert config.filename.name == 'pybo.yaml'  # config.filename is a Path object
-
-# paths for trie content
-main, custom = config.get_tok_data_paths('POS')
-# each profile contains one or more sections
-assert [m for m in main] == ['lexica_bo', 'pos']
-# each element in a Path object leading to a resource file
-assert isinstance(main['pos'][0], Path)
-
-# custom files to overwrite the existing trie can be added as follows
-assert len(custom) == 0
-main, custom = config.get_tok_data_paths('POS', modifs='./pybo/tests/trie_data/')
-assert [c for c in custom] == ['lexica_bo', 'lemmas'] == [t.parts[-1] for t in Path('./pybo/tests/trie_data/').glob('*')]
-
-# overwriting the main profile
-main, custom = config.get_tok_data_paths('./pybo/tests/trie_data/', mode='custom')
-assert [m for m in main] == ['lexica_bo', 'lemmas']
+# ##########################################################################################################
+# # test_config
+# ##########################################################################################################
+#
+# # def test_config():
+# config = Config()
+#
+# # default config filename
+# assert config.filename.name == 'pybo.yaml'  # config.filename is a Path object
+#
+# # paths for trie content
+# main, custom = config.get_tok_data_paths('POS')
+# # each profile contains one or more sections
+# assert [m for m in main] == ['lexica_bo', 'pos']
+# # each element in a Path object leading to a resource file
+# assert isinstance(main['pos'][0], Path)
+#
+# # custom files to overwrite the existing trie can be added as follows
+# assert len(custom) == 0
+# main, custom = config.get_tok_data_paths('POS', modifs='./pybo/tests/trie_data/')
+# assert [c for c in custom] == ['lexica_bo', 'lemmas'] == [t.parts[-1] for t in Path('./pybo/tests/trie_data/').glob('*')]
+#
+# # overwriting the main profile
+# main, custom = config.get_tok_data_paths('./pybo/tests/trie_data/', mode='custom')
+# assert [m for m in main] == ['lexica_bo', 'lemmas']
 
 ##########################################################################################################
 # test_splitaffixed.py
