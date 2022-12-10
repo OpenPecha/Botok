1188b20eb86ccec2646e75752e40a019ef41226b
eroux <elie.roux@telecom-bretagne.eu>
2022-12-10 12:32:50 +0100

    polish corner cases, document

--- botok/tokenizers/unicode_normalizer.py
+++ botok/tokenizers/unicode_normalizer.py
@@ -1,39 +1,88 @@
 import re
 from enum import Enum
 
-def normalize_unicode(s, normalization_type="graphical"):
-    # combined characters, using NFD representation:
-    s = s.replace("\u0f00", "\u0f68\u0f7c\u0f7e")
-    s = s.replace("\u0f43", "\u0f42\u0fb7")
-    s = s.replace("\u0f4d", "\u0f4c\u0fb7")
-    s = s.replace("\u0f52", "\u0f51\u0fb7")
-    s = s.replace("\u0f57", "\u0f56\u0fb7")
-    s = s.replace("\u0f5c", "\u0f5b\u0fb7")
-    s = s.replace("\u0f69", "\u0f40\u0fb5")
-    s = s.replace("\u0f73", "\u0f71\u0f72")
-    s = s.replace("\u0f75", "\u0f71\u0f74")
-    s = s.replace("\u0f76", "\u0fb2\u0f80")
-    s = s.replace("\u0f77", "\u0fb2\u0f71\u0f80")
-    s = s.replace("\u0f78", "\u0fb3\u0f80")
-    s = s.replace("\u0f79", "\u0fb3\u0f71\u0f80")
-    s = s.replace("\u0f81", "\u0f71\u0f80")
-    s = s.replace("\u0f93", "\u0f92\u0fb7")
-    s = s.replace("\u0f9d", "\u0f9c\u0fb7")
-    s = s.replace("\u0fa2", "\u0fa1\u0fb7")
-    s = s.replace("\u0fa7", "\u0fa6\u0fb7")
-    s = s.replace("\u0fac", "\u0fab\u0fb7")
-    s = s.replace("\u0fb9", "\u0f90\u0fb5")
-    if normalization_type == "graphical":
+def normalize_unicode(s, form="nfd", normalize_graphical = True, normalize_typography = False):
+    # first, unify Unicode form:
+    # http://www.unicode.org/faq/normalization.html
+    # https://unicode.org/reports/tr15/
+    # https://unicode.org/charts/normalization/chart_Tibetan.html
+    # although for some reason this chart considers 0f0c -> 0f0b in NFD
+    if form == "nfd":
+        s = s.replace("\u0f43", "\u0f42\u0fb7")
+        s = s.replace("\u0f4d", "\u0f4c\u0fb7")
+        s = s.replace("\u0f52", "\u0f51\u0fb7")
+        s = s.replace("\u0f57", "\u0f56\u0fb7")
+        s = s.replace("\u0f5c", "\u0f5b\u0fb7")
+        s = s.replace("\u0f69", "\u0f40\u0fb5")
+        s = s.replace("\u0f73", "\u0f71\u0f72") # use is discouraged
+        s = s.replace("\u0f75", "\u0f71\u0f74") # use is discouraged
+        s = s.replace("\u0f76", "\u0fb2\u0f80")
+        s = s.replace("\u0f77", "\u0fb2\u0f71\u0f80") # deprecated
+        s = s.replace("\u0f78", "\u0fb3\u0f80")
+        s = s.replace("\u0f79", "\u0fb3\u0f71\u0f80") # deprecated
+        s = s.replace("\u0f81", "\u0f71\u0f80") # use is discouraged
+        s = s.replace("\u0f93", "\u0f92\u0fb7")
+        s = s.replace("\u0f9d", "\u0f9c\u0fb7")
+        s = s.replace("\u0fa2", "\u0fa1\u0fb7")
+        s = s.replace("\u0fa7", "\u0fa6\u0fb7")
+        s = s.replace("\u0fac", "\u0fab\u0fb7")
+        s = s.replace("\u0fb9", "\u0f90\u0fb5")
+    else:
+        s = s.replace("\u0f42\u0fb7", "\u0f43")
+        s = s.replace("\u0f4c\u0fb7", "\u0f4d")
+        s = s.replace("\u0f51\u0fb7", "\u0f52")
+        s = s.replace("\u0f56\u0fb7", "\u0f57")
+        s = s.replace("\u0f5b\u0fb7", "\u0f5c")
+        s = s.replace("\u0f40\u0fb5", "\u0f69")
+        s = s.replace("\u0f73", "\u0f71\u0f72") # use is discouraged
+        s = s.replace("\u0f75", "\u0f71\u0f74") # use is discouraged
+        s = s.replace("\u0fb2\u0f80", "\u0f76")
+        s = s.replace("\u0f77", "\u0fb2\u0f71\u0f80") # deprecated
+        s = s.replace("\u0fb3\u0f80", "\u0f78")
+        s = s.replace("\u0f79", "\u0fb3\u0f71\u0f80") # deprecated
+        s = s.replace("\u0f81", "\u0f71\u0f80") # use is discouraged
+        s = s.replace("\u0f92\u0fb7", "\u0f93")
+        s = s.replace("\u0f9c\u0fb7", "\u0f9d")
+        s = s.replace("\u0fa1\u0fb7", "\u0fa2")
+        s = s.replace("\u0fa6\u0fb7", "\u0fa7")
+        s = s.replace("\u0fab\u0fb7", "\u0fac")
+        s = s.replace("\u0f90\u0fb5", "\u0fb9")
+    if normalize_graphical:
+        # These substitutions normalize things that have the same
+        # graphical representation
+        #
+        # no graphical distinction between 0f0b and 0f0c
         s = s.replace("\u0f0c", "\u0f0b")
-        s = s.replace("\u0f38", "\u0f27")
+        # double shad is just two shad
         s = s.replace("\u0f0e", "\u0f0d\u0f0d")
-    # ra does't transform into a small rago before nya
-    s = s.replace("\u0f65\u0f99", "\u0f62\u0f99")
-    # no achung in the middle of stacks, only full a
-    s = re.sub(r"[\u0f71]([\u0f8d-\u0fbc])", "\u0fb0\\1", s)
-    # no full a at the end of stacks, only achung
-    s = re.sub(r"[\u0fb0]([^\u0f8d-\u0fbc]|$)", "\u0f71\\1", s)
-    # reorder
+        # the distinction between 0f38 and 0f27 is semantic but rarely
+        # distinguished graphically and often completely missed by inputters
+        s = s.replace("\u0f38", "\u0f27")
+        # 0f00 has not been marked as a composed character in Unicode
+        # This is something that they now admit is a mistake, but it cannot be
+        # changed because of Unicode change policies.
+        s = s.replace("\u0f00", "\u0f68\u0f7c\u0f7e")
+        # /!\ some fonts don't display these combinations in the exact same way
+        # but since there's no semantic distinction and the graphical variation
+        # is unclear, it seems safe
+        s = s.replace("\u0f7a\u0f7a", "\u0f7b")
+        s = s.replace("\u0f7c\u0f7c", "\u0f7d")
+        # ra does't transform into a small rago before nya, so using 0f65
+        # does not change its graphical representation in that case
+        s = s.replace("\u0f65\u0f99", "\u0f62\u0f99")
+        # the diference between 0f71 and 0fb0 is often very ambiguous when
+        # looking at original sources. We normalize them in order to
+        # make the data coherent:
+        # no 0f71 in the middle of stacks, only 0fb0
+        s = re.sub(r"[\u0f71]([\u0f8d-\u0fac\u0fae\u0fb0\u0fb3-\u0fbc])", "\u0fb0\\1", s)
+        # no 0fb0 at the end of stacks, only 0f71
+        s = re.sub(r"[\u0fb0]([^\u0f8d-\u0fac\u0fae\u0fb0\u0fb3-\u0fbc]|$)", "\u0f71\\1", s)
+        # things we do not normalize:
+        # 0f74+0f71 -> 0f71+0f74, because the combination appears sometimes in the sources
+        # for instance སུྰ in https://adarsha.dharma-treasure.org/kdbs/jiangkangyur/pbs/2618229
+        # same for 0fb1+0f71 since the combination also appears
+        # for instance སཱྱ on https://adarsha.dharma-treasure.org/kdbs/jiangkangyur?pbId=2627013
+        # 0f05 must follow a 0f04, otherwise transform into 0f04? (same for 0fd4 0fd3)
     s, valid = reorder_bo(s)
     return s, valid
 
@@ -41,43 +90,46 @@ class Cats(Enum):
     Other = 0
     Base = 1
     Subscript = 2
-    Achung = 3
-    Chabkyu = 4
-    BottomMark = 5
-    TopVowel = 6
-    TopMark = 7
-    RightMark = 8
+    BottomVowel = 3
+    BottomMark = 4
+    TopVowel = 5
+    TopMark = 6
+    RightMark = 7
 
-CATEGORIES =  ([Cats.Other]          # 0F00
-             + [Cats.Base]           # 0F01, kind of cheating for diacritics
-             + [Cats.Other] * 51     # 0F02-0F34
-             + [Cats.BottomMark]     # 0F35
-             + [Cats.Other]          # 0F36
-             + [Cats.BottomMark]     # OF37
-             + [Cats.Other]          # 0F38
-             + [Cats.Subscript]      # 0F39, kind of cheating but works
-             + [Cats.Other] * 4      # 0F3A-0F3D
-             + [Cats.RightMark]      # 0F3E
-             + [Cats.Other]          # 0F3F, not quite sure
-             + [Cats.Base] * 45      # 0F40-0F6C
-             + [Cats.Other] * 4      # 0F6D-0F70
-             + [Cats.Achung]         # 0F71
-             + [Cats.TopVowel]       # 0F72
-             + [Cats.Achung]         # 0F73
-             + [Cats.Chabkyu] * 2    # 0F74-0F75
-             + [Cats.TopVowel] * 8   # 0F76-0F7D
-             + [Cats.TopMark]        # 0F7E
-             + [Cats.RightMark]      # 0F7F
-             + [Cats.TopVowel] * 2   # 0F80-0F81
-             + [Cats.TopMark] * 2    # 0F82-0F83
-             + [Cats.BottomMark]     # 0F84
-             + [Cats.Other]          # 0F85
-             + [Cats.TopMark] * 2    # 0F86-0F87
-             + [Cats.Base] * 2       # 0F88-0F89
-             + [Cats.Other] * 2      # 0F8A-0F8B
-             + [Cats.Base]           # 0F8C
-             + [Cats.Subscript] * 48 # 0F8D-0FBC
-             + [Cats.Subscript] * 48 # 0F8D-0FBC
+CATEGORIES =  ([Cats.Other]           # 0F00
+             + [Cats.Base]            # 0F01, often followed by 0f083
+             + [Cats.Other] * 22      # 0F02-0F17
+             + [Cats.BottomVowel] * 2 # 0F18-0F19
+             + [Cats.Other] * 6       # 0F1A-0F1F
+             + [Cats.Base] * 20       # 0F20-0F33, numbers can be followed by 0f18, 0f19 or exceptionally by vowels
+             + [Cats.Other]           # 0F34
+             + [Cats.BottomMark]      # 0F35
+             + [Cats.Other]           # 0F36
+             + [Cats.BottomMark]      # OF37
+             + [Cats.Other]           # 0F38
+             + [Cats.Subscript]       # 0F39, kind of cheating but works
+             + [Cats.Other] * 4       # 0F3A-0F3D
+             + [Cats.RightMark]       # 0F3E
+             + [Cats.Other]           # 0F3F, not quite sure
+             + [Cats.Base] * 45       # 0F40-0F6C
+             + [Cats.Other] * 4       # 0F6D-0F70
+             + [Cats.BottomVowel]     # 0F71
+             + [Cats.TopVowel]        # 0F72
+             + [Cats.TopVowel]        # 0F73
+             + [Cats.BottomVowel] * 2 # 0F74-0F75
+             + [Cats.TopVowel] * 8    # 0F76-0F7D
+             + [Cats.TopMark]         # 0F7E
+             + [Cats.RightMark]       # 0F7F
+             + [Cats.TopVowel] * 2    # 0F80-0F81
+             + [Cats.TopMark] * 2     # 0F82-0F83
+             + [Cats.BottomMark]      # 0F84
+             + [Cats.Other]           # 0F85
+             + [Cats.TopMark] * 2     # 0F86-0F87
+             + [Cats.Base] * 2        # 0F88-0F89
+             + [Cats.Base]            # 0F8A always followed by 0f82 (required by the Unicode spec)
+             + [Cats.Other]           # 0F8B
+             + [Cats.Base]            # 0F8C
+             + [Cats.Subscript] * 48  # 0F8D-0FBC
              )
 
 def charcat(c):
@@ -128,17 +180,26 @@ def debug_to_unicode(s):
 
 def assert_conv(orig, expected, expectedValid = True):
     resultStr, resultValid = normalize_unicode(orig)
-    assert(resultStr == expected)
-    assert(resultValid == expectedValid)
+    assert resultStr == expected, "%s -> %s but %s expected" % (debug_to_unicode(orig), debug_to_unicode(resultStr), debug_to_unicode(expected))
+    assert resultValid == expectedValid, "%s valid? -> %s but %s expected" % (debug_to_unicode(orig), resultValid, expectedValid)
 
 def test_normalize_unicode():
     assert_conv("\u0f77", "\u0fb2\u0f71\u0f80", False)
-    assert_conv("\u0f40\u0f7e\u0f7c\u0f74\u0f71", "\u0f40\u0f71\u0f74\u0f7c\u0f7e")
-    #assert_conv("\u0f66\u0f71\u0fb1", "\u0f66\u0fb1\u0f71")
-    assert_conv("\u0f58\u0f74\u0fb0\u0f83", "\u0f58\u0f71\u0f74\u0f83")
-    assert_conv("\u0F51\u0FB7\u0F74\u0FB0", "\u0F51\u0FB7\u0f71\u0F74")
+    assert_conv("\u0f40\u0f7e\u0f7c\u0f74\u0f71", "\u0f40\u0f74\u0f71\u0f7c\u0f7e")
+    assert_conv("\u0f58\u0f74\u0fb0\u0f83", "\u0f58\u0f74\u0f71\u0f83")
+    assert_conv("\u0F51\u0FB7\u0F74\u0FB0", "\u0F51\u0FB7\u0F74\u0f71")
     assert_conv("\u0F66\u0F7C\u0FB1", "\u0F66\u0FB1\u0F7C")
     assert_conv("\u0F0B\u0F7E", "\u0F0B\u0F7E", False)
+    assert_conv("\u0f65\u0f99\u0f7a\u0f7a", "\u0f62\u0f99\u0f7b")
+    assert_conv("\u0f01\u0f83", "\u0f01\u0f83") # should be valid
 
 if __name__ == "__main__":
     test_normalize_unicode()
+    #with open("allstacks.txt") as f:
+    #    for l in f.readlines():
+    #        l = l[:-1]
+    #        res, valid = normalize_unicode(l)
+    #        if l != res:
+    #            print("transform '%s' into '%s'" % (l, res))
+    #        if not valid:
+    #            print("'%s' not valid" % l)
